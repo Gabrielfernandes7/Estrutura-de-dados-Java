b2497bc87c2e4a84841d9c91dcd6c960fc161075
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2024-04-26 16:26:01 -0300

    finalizacao da Busca Binaria e edicao da Busca Linear

--- Capitulo02/BuscaBinaria.java
+++ Capitulo02/BuscaBinaria.java
@@ -5,17 +5,27 @@ public class BuscaBinaria {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // lista ordenada
         int[] numeros = { 1, 2, 4, 5, 6, 9, 10 };
-        int meio = numeros.length;
-
+        
+        int indiceInferior = 0;
+        int indiceSuperior = numeros.length - 1;
+        
         System.out.println("Digite o número que você deseja obter");
         int numeroBuscado = input.nextInt();
+        
+        while (indiceInferior <= indiceSuperior) {
+            int indiceMeio = (indiceSuperior + indiceInferior) / 2;
 
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeroBuscado == numeros[meio]) {
-                System.out.print("O número " + numeroBuscado + " foi encontrado na lista");
-            } else if (numeroBuscado > numeros[meio]) {
-                
+            if (numeroBuscado == numeros[indiceMeio]) {
+                System.out.println("Número encontrado na posição " + indiceMeio);
+                break;
+            } else if (numeroBuscado < numeros[indiceMeio]) {
+                indiceSuperior = indiceMeio - 1;
+            } else if (numeroBuscado > numeros[indiceMeio]) {
+                indiceInferior = indiceMeio + 1;
+            } else {
+                System.out.println("Número não encontrado");
             }
         }
 
--- Capitulo02/BuscaLinear.java
+++ Capitulo02/BuscaLinear.java
@@ -7,15 +7,15 @@ public class BuscaLinear {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
-        int[] numeros = { 1, 2, 3, 4, 5, 6, 7};
+        int[] numeros = { 1, 2, 3, 4, 5, 6, 7 };
 
         System.out.println("Digite um valor que você deseja buscar na lista");
         int numeroBuscado = input.nextInt();
         
        for (int i = 0; i < numeros.length; i++) {
-        if (numeroBuscado == numeros[i]) {
-            System.out.println("O número " + numeroBuscado + " foi encontrado na posição " + i);
-        }
+            if (numeroBuscado == numeros[i]) {
+                System.out.println("O número " + numeroBuscado + " foi encontrado na posição " + i);
+            }
        }
 
         input.close();
