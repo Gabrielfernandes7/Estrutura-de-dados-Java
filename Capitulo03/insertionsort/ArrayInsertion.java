184f0776df9e8bcf31f8e4f5dd6a482cbceb6991
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2024-05-01 21:21:42 -0300

    construtor insertionsort

---
+++ Capitulo03/insertionsort/ArrayInsertion.java
@@ -0,0 +1,11 @@
+package Capitulo03.insertionsort;
+
+public class ArrayInsertion {
+    private long[] vetorA;
+    private int numeroElementos;
+
+    public ArrayInsertion(int max) {
+        vetorA = new long[max];
+        numeroElementos = 0;
+    }
+}
--- Capitulo03/insertionsort/InsertionSortMain.java
+++ Capitulo03/insertionsort/InsertionSortMain.java
@@ -1,3 +1,5 @@
+package Capitulo03.insertionsort;
+
 public class InsertionSortMain {
     public static void main(String[] args) {
         
